591d46237586280a51176342a49b2fd10919003d
juanestebanchaparromachete <je.chaparro10@gmail.com>
2016-09-01 11:19:54 -0500

    maven
    change to maven

---
+++ ShaEstudio/src/main/java/com/java/ex/mundo/ShoppingCar.java
@@ -0,0 +1,56 @@
+package com.java.ex.mundo;
+
+import java.util.ArrayList;
+
+public class ShoppingCar {
+	
+	/**
+	 * total shopping car 
+	 */
+	private double total;
+	
+	/**
+	 * array of items 
+	 */
+	private ArrayList<Item> itemsShoppingCar;
+	
+	/**
+	 * Shopping car user 
+	 */
+	private User user ;
+
+	public ShoppingCar(double total, ArrayList<Item> itemsShoppingCar, User user) {
+		super();
+		this.total = total;
+		this.itemsShoppingCar = itemsShoppingCar;
+		this.user = user;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public ArrayList<Item> getItemsShoppingCar() {
+		return itemsShoppingCar;
+	}
+
+	public void setItemsShoppingCar(ArrayList<Item> itemsShoppingCar) {
+		this.itemsShoppingCar = itemsShoppingCar;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	
+
+}
---
+++ ShaEstudio/src/main/java/com/java/ex/mundo/Store.java
@@ -0,0 +1,6 @@
+package com.java.ex.mundo;
+
+public class Store {
+
+	
+}
