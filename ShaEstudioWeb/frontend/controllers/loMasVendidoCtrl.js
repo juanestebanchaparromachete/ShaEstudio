9c00e28d745fec20f00e78a37949ca4072e7b565
juanestebanchaparromachete <je.chaparro10@gmail.com>
2017-01-17 23:53:06 -0500

    last changes

--- ShaEstudioWeb/frontend/controllers/loMasVendidoCtrl.js
+++ ShaEstudioWeb/frontend/controllers/loMasVendidoCtrl.js
@@ -1,23 +1,132 @@
-app.controller("loMasVendidoCtrl", function ($scope , shareProduct) {
+app.controller("loMasVendidoCtrl", function ($scope, shareProduct) {
     var productos = $scope.productos = [{
-            nombre: 'top seller 1',
-            imagen: 'images/image4.jpg',
-            descripcion: 'Mueble espacioso y con buenas proporciones',
-            id: 5435
+        nombre: 'producto1',
+        imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+        descripcion: 'Mueble espacioso y con buenas proporciones',
+        id: 1001,
+        disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
     },
-        {
-            nombre: 'producto2',
-            imagen: 'images/image3.jpg',
-            descripcion: 'Mueble espacioso y con buen color ',
-            id: 7585
+                                        {
+                                            nombre: 'producto2',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buen color ',
+                                            id: 1002,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        }, {
+                                            nombre: 'producto3',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buenas proporciones',
+                                            id: 1003,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        },
+                                        {
+                                            nombre: 'producto4',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buen color ',
+                                            id: 1004,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        }, {
+                                            nombre: 'producto5',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buenas proporciones',
+                                            id: 1005,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        },
+                                        {
+                                            nombre: 'producto6',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buen color ',
+                                            id: 1006,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        }, {
+                                            nombre: 'producto7',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buenas proporciones',
+                                            id: 1007,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        },
+                                        {
+                                            nombre: 'producto8',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buen color ',
+                                            id: 1008,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        }, {
+                                            nombre: 'producto9',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buenas proporciones',
+                                            id: 1009,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        },
+                                        {
+                                            nombre: 'producto10',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg']
+                                            ,descripcion: 'Mueble espacioso y con buen color ',
+                                            id: 1010,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        }, {
+                                            nombre: 'producto11',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buenas proporciones',
+                                            id: 1011,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        },
+                                        {
+                                            nombre: 'producto12',
+                                            imagenes: ['images/pruebafoto.jpg',
+                                                       'images/pruebafoto.jpg',
+                                                       'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buen color ',
+                                            id: 1012,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        }, {
+                                            nombre: 'producto13',
+                                            imagenes: ['images/pruebafoto.jpg',
+                                                       'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buenas proporciones',
+                                            id: 1013,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        },
+                                        {
+                                            nombre: 'product14',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buen color ',
+                                            id: 1014,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        }, {
+                                            nombre: 'producto15',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buenas proporciones',
+                                            id: 1015,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        },
+                                        {
+                                            nombre: 'producto16',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buen color ',
+                                            id: 1016,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        }, {
+                                            nombre: 'producto17',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buenas proporciones',
+                                            id: 1017,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
+                                        },
+                                        {
+                                            nombre: 'producto18',
+                                            imagenes: ['images/pruebafoto.jpg', 'images/pruebafoto.jpg', 'images/pruebafoto.jpg'],
+                                            descripcion: 'Mueble espacioso y con buen color ',
+                                            id: 1018,
+                                            disenador: 'disenador a x j a ', fechaCreacion:'dia de mes de año'
                                         }];
     $scope.passObject = function (idObject) {
 
         for (var i = 0; i < productos.length; i++) {
 
             if (productos[i].id == idObject) {
-                console.log( productos[i].id );
-                shareProduct.setObject(productos[i]); 
+                console.log(productos[i].id);
+                shareProduct.setObject(productos[i]);
 
             }
 
@@ -25,4 +134,20 @@ app.controller("loMasVendidoCtrl", function ($scope , shareProduct) {
 
     }
 
+    $scope.viewby = 3;
+    $scope.bigTotalItems = 18;
+    $scope.bigCurrentPage = 1;
+    $scope.itemsPerPage = $scope.viewby;
+    $scope.maxSize = 10;
+    $scope.numPages= Math.ceil( $scope.bigTotalItems / $scope.itemsPerPage);
+    console.log( $scope.numPages);
+
+    $scope.setPage = function (pageNo) {
+        $scope.currentPage = pageNo;
+
+    };
+
+    $scope.pageChanged = function() {
+        console.log('Page changed to: ' + $scope.currentPage);
+    };
 });
--- ShaEstudioWeb/frontend/controllers/productoCtrl.js
+++ ShaEstudioWeb/frontend/controllers/productoCtrl.js
@@ -1,10 +1,31 @@
 app.controller("productoCtrl", function ($scope, shareProduct) {
-    $scope.producto = {
-        nombre: shareProduct.getObject().nombre,
-        descripcion: shareProduct.getObject().descripcion,
-        imagen: shareProduct.getObject().imagen,
-        id: shareProduct.getObject().id
-    }
+    
+        
+    var objectValue = { };
+    objectValue = shareProduct.getObject();
+    $scope.producto = objectValue;
+    $scope.images= objectValue.imagenes;
+    console.log($scope.images);
+    
+    $scope.viewby = 1;
+    $scope.bigTotalItems = 3;
+    $scope.bigCurrentPage = 1;
+    $scope.itemsPerPage = $scope.viewby;
+    $scope.maxSize = 10;
+    $scope.numPages = Math.ceil($scope.bigTotalItems / $scope.itemsPerPage);
+    console.log($scope.numPages);
 
+    $scope.setPage = function (pageNo) {
+        $scope.currentPage = pageNo;
+
+    };
+
+    $scope.pageChanged = function () {
+        console.log('Page changed to: ' + $scope.currentPage);
+    };
+    
+    function goBack() {
+        window.history.back();
+    }
 
 });
